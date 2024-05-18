5a7d982412b8d6dae8642e9fbdee443aeda6d38a
root <root@gubaoer-pcx>
2024-05-18 22:37:46 +0800

    modify regal_test.go

--- regal_test.go
+++ regal_test.go
@@ -1,8 +1,30 @@
 package regal
 
 import (
+	"strings"
 	"testing"
 )
+
+func TestRegalEngine(t *testing.T) {
+	combine := 2
+	schedule := 3
+	var example1 = [][]string{
+		{"app-test-version1.0", "10.1.1.1,10.1.1.2,10.1.1.3,10.1.1.4,10.1.1.5"},
+	}
+
+	if ans := RegalEngine(example1).Grouping(); len(ans) == 0 {
+		t.Errorf("internal error")
+	}
+
+	if ans := RegalEngine(example1, WithCombine(combine), WithSchedule(schedule)).Grouping(); len(ans[0][1].([][]string)[1]) != combine {
+		t.Errorf("combine expected be %d", combine)
+	}
+
+	if ans := RegalEngine(example1, WithCombine(combine), WithSchedule(schedule)).Grouping(); len(strings.Split(ans[0][1].([][]string)[0][0], ",")) != schedule {
+		t.Errorf("schedule expected be %d", schedule)
+	}
+
+}
 
 func BenchmarkRegalEngine(b *testing.B) {
 
